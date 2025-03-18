6fece3267a0502f3b5f0105070267c77c9f4ece6
Stepan Shilin <s.shilin@cuberto.com>
2025-03-18 18:52:49 +0400

    add image for volume indicator, add fade to black filter

--- src/animation_node.rs
+++ src/animation_node.rs
@@ -1,12 +1,14 @@
-use core::usize;
-
+use bumpalo::Bump;
+use core::cell::RefCell;
 use embedded_graphics::image::Image;
-use embedded_graphics::pixelcolor::Gray4;
+use embedded_graphics::pixelcolor::{Rgb555, Rgb565, Rgb888};
 use embedded_graphics::prelude::*;
+use heapless::Vec;
 use libm::{round, sin};
 use tinybmp::Bmp;
 
-const PI: f64 = 3.14159265358979323846;
+pub const PI: f64 = 3.14159265358979323846;
+pub const CHILD_COUNT: usize = 16;
 
 fn abs_sin(frame: u32, duration: u32, start: Point, end: Point, freq: Point) -> Point {
     let tp: f64 = frame as f64 / duration as f64 * PI;
@@ -18,52 +20,115 @@ fn abs_sin(frame: u32, duration: u32, start: Point, end: Point, freq: Point) -> 
     Point::new(x.abs(), y.abs())
 }
 
-pub struct AnimationNode<'d> {
+pub struct BoneBlueprint {
+    pub max: Point,
+    pub start: Point,
+    pub end: Point,
+    pub freq: Point,
+    pub duration: u32,
+    pub sprite: &'static [u8],
+    pub children: &'static [BoneBlueprint],
+}
+
+pub struct Skeleton<'d, C, const N: usize>
+where
+    C: PixelColor + From<Rgb555> + From<Rgb565> + From<Rgb888>,
+{
+    pub arena: &'d Bump,
+    pub root_bone: &'d RefCell<Bone<'d, C, N>>,
+}
+
+impl<'d, C, const N: usize> Skeleton<'d, C, N>
+where
+    C: PixelColor + From<Rgb555> + From<Rgb565> + From<Rgb888>,
+{
+    pub fn from_blueprint(bone_blueprint: &BoneBlueprint, arena: &'d Bump) -> Self {
+        let root_bone = Bone::<C, N>::from_blueprint(bone_blueprint, &arena);
+
+        Self { arena, root_bone }
+    }
+
+    pub fn update(&mut self) {
+        self.root_bone.borrow_mut().update();
+    }
+
+    pub fn draw<D>(&self, display: &mut D) -> Result<(), D::Error>
+    where
+        D: DrawTarget<Color = C>,
+    {
+        self.root_bone.borrow().draw(display)
+    }
+}
+
+pub struct Bone<'d, C, const N: usize>
+where
+    C: PixelColor + From<Rgb555> + From<Rgb565> + From<Rgb888>,
+{
     pub max: Point,
     pub coords: Point,
+    next_coords: Point,
     pub start: Point,
     pub end: Point,
     pub freq: Point,
     pub frame: u32,
     pub duration: u32,
-    pub sprite: Image<'d, Bmp<'d, Gray4>>,
+    sprite: Bmp<'d, C>,
+    children: Vec<&'d RefCell<Bone<'d, C, N>>, N>,
 }
 
-impl<'d> AnimationNode<'d> {
-    pub fn new(
-        start: Point,
-        end: Point,
-        max: Point,
-        freq: Point,
-        duration: u32,
-        sprite: Image<'d, Bmp<'d, Gray4>>,
-    ) -> Self {
-        Self {
+impl<'d, C, const N: usize> Bone<'d, C, N>
+where
+    C: PixelColor + From<Rgb555> + From<Rgb565> + From<Rgb888>,
+{
+    pub fn from_blueprint(bone_blueprint: &BoneBlueprint, arena: &'d Bump) -> &'d RefCell<Self> {
+        let children = bone_blueprint
+            .children
+            .iter()
+            .map(|child_bp| Bone::from_blueprint(child_bp, arena))
+            .collect::<heapless::Vec<_, N>>();
+
+        let bone = Self {
             frame: 1,
-            duration,
-            coords: Point::new(1, 1),
-            start,
-            end,
-            max,
-            freq,
-            sprite,
-        }
+            duration: bone_blueprint.duration,
+            coords: bone_blueprint.start.clone(),
+            next_coords: bone_blueprint.start.clone(),
+            start: bone_blueprint.start,
+            end: bone_blueprint.end,
+            max: bone_blueprint.max,
+            freq: bone_blueprint.freq,
+            sprite: Bmp::from_slice(bone_blueprint.sprite).unwrap(),
+            children,
+        };
+
+        arena.alloc(RefCell::new(bone))
     }
 
     pub fn update(&mut self) {
-        self.coords = abs_sin(self.frame, self.duration, self.start, self.end, self.freq);
+        self.next_coords = abs_sin(self.frame, self.duration, self.start, self.end, self.freq);
 
         self.frame = if self.frame >= self.duration {
             1
         } else {
             self.frame + 1
         };
+
+        self.coords = self.next_coords;
+
+        for child_bone in &self.children {
+            child_bone.borrow_mut().update();
+        }
     }
 
     pub fn draw<D>(&self, display: &mut D) -> Result<(), D::Error>
     where
-        D: DrawTarget<Color = Gray4>,
+        D: DrawTarget<Color = C>,
     {
-        self.sprite.draw(display)
+        let image = Image::new(&self.sprite, self.coords);
+
+        for child_bone in &self.children {
+            child_bone.borrow().draw(display).ok();
+        }
+
+        image.draw(display)
     }
 }
--- src/graphics.rs
+++ src/graphics.rs
@@ -1,38 +1,53 @@
+// use bumpalo::Bump;
 use embassy_time::{Duration, Ticker};
 use embedded_graphics::pixelcolor::Gray4;
 use embedded_graphics::prelude::*;
 
 const FRAME_DELAY: u64 = 20;
 
-use crate::animation_node::AnimationNode;
+// use crate::animation_node::BoneBlueprint;
+// use crate::animation_node::Skeleton;
+// use crate::animation_node::CHILD_COUNT;
 use crate::screen;
+use crate::volume_indicator::VolumeIndicator;
+
+// const SKELETON: BoneBlueprint = BoneBlueprint {
+//     start: Point::new(83, 0),
+//     end: Point::new(87, 10),
+//     max: Point::new(screen::SCREEN_WIDTH as i32, screen::SCREEN_HEIGHT as i32),
+//     freq: Point::new(1, 2),
+//     duration: 100,
+//     sprite: include_bytes!("sprites/parts/outline.bmp"),
+//     children: &[BoneBlueprint {
+//         start: Point::new(0, 0),
+//         end: Point::new(0, 0),
+//         max: Point::new(screen::SCREEN_WIDTH as i32, screen::SCREEN_HEIGHT as i32),
+//         freq: Point::new(1, 2),
+//         duration: 100,
+//         sprite: include_bytes!("sprites/parts/jaw.bmp"),
+//         children: &[],
+//     }],
+// };
 
 #[embassy_executor::task]
 pub async fn prepare_frame_task() {
-    let image_data = include_bytes!(sprite_location);
-
-    let bmp: Bmp<Gray4> = Bmp::from_slice(data).unwrap();
-
-    let image = Image::new(&bmp, self.coords);
-
-    let mut animation_root = AnimationNode::new(
-        Point::new(83, 0),
-        Point::new(87, 10),
-        Point::new(screen::SCREEN_WIDTH as i32, screen::SCREEN_HEIGHT as i32),
-        Point::new(1, 2),
-        100,
-        "sprites/parts/outline.bmp",
-    );
+    // let arena = Bump::new();
+    // let mut skeleton: Skeleton<'_, Gray4, CHILD_COUNT> =
+    //     Skeleton::from_blueprint(&SKELETON, &arena);
 
     let mut ticker = Ticker::every(Duration::from_millis(FRAME_DELAY));
+
+    let indicator: VolumeIndicator = VolumeIndicator::new(Point::new(5, 15));
 
     loop {
         let frame = screen::NEXT_FRAME.wait().await;
         frame.clear(Gray4::BLACK).unwrap();
 
-        animation_root.update();
+        // skeleton.update();
 
-        animation_root.draw(frame).unwrap();
+        // skeleton.draw(frame).ok();
+
+        indicator.draw(frame);
 
         screen::READY_FRAME.signal(frame);
 
